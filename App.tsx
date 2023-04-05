e534731a01b556fa2181ad00e2b8634bd92b3c74
Ardian Effendi <ardian.effendi21@gmail.com>
2023-04-05 22:05:29 +0800

    init app and add functionality to HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
-export default function App() {
+export default function App(): JSX.Element {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeHolderImage={require("./assets/images/background-image.png")}
+          selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button theme="" label="Use this photo" onPress={pickImageAsync} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +45,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
---
+++ components/ImageViewer.tsx
@@ -0,0 +1,23 @@
+import { Image, ImageSourcePropType, StyleSheet } from "react-native";
+
+type ImageProp = {
+  placeHolderImage: ImageSourcePropType;
+  selectedImage: string;
+};
+
+const ImageViewer = ({ placeHolderImage, selectedImage }: ImageProp) => {
+  const imageSource =
+    selectedImage !== null ? { uri: selectedImage } : placeHolderImage;
+
+  return <Image source={imageSource} style={styles.image} />;
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
+
+export default ImageViewer;
